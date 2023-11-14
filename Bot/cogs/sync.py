e84f08e21ce150c869f06f28daa2f36c0a8321f4
will <williamlogan77@gmail.com>
2023-11-14 19:41:19 +0000

    revisted cog to convert to group cog

--- Bot/cogs/sync.py
+++ Bot/cogs/sync.py
@@ -1,39 +1,55 @@
 import discord
-from discord.ext import commands
-from discord import app_commands
+from discord.ext.commands import GroupCog
+from discord.app_commands import command
 import glob
 import os
 
+from Bot.main import MyDiscordBot
 
-class Sync(commands.Cog):
 
-    def __init__(self, bot: commands.Bot):
+class Refresh(
+    GroupCog,
+    group_name="Refresh",
+    group_description="Controls the refreshing and syncing",
+):
+    def __init__(self, bot: MyDiscordBot):
         self.bot = bot
 
-    @app_commands.command(name="sync")
+    @command(name="sync")
     async def sync(self, ctx: discord.Interaction):
         await ctx.response.defer()
-        msg = await ctx.followup.send("Syncing cogs...",
-                                      wait=True,
-                                      ephemeral=True)
-        user_roles = [x.name for x in ctx.user.roles]  # type: ignore
-        if "Keeper of Chomage" in user_roles:
-            print("updating cogs", flush=True)
-            os.chdir("cogs/")
-            for cog in glob.glob("*.py"):
-                if not cog.startswith("sync"):
-                    print("Reloading", cog, flush=True)
-                    await self.bot.reload_extension(f"cogs.{cog[:-3]}")
-            os.chdir("../")
+        msg = await ctx.followup.send("Syncing commands...", wait=True, ephemeral=True)
+        self.bot.logging.info("Syncing commands")
+        try:
             await self.bot.tree.sync()
-            await msg.edit(content="Sucessfully synced and reloaded all cogs.")
-            self.bot.logging.info("Synced and reloaded all cogs")
-        else:
-            msg.edit(
-                content=
-                "You need the 'Keeper of Chomage' Role to use this command")
-            self.bot.logging.warning("Cogs have not reloaded")
+            msg.edit(content="Succesfully synced")
+        except Exception as e:
+            self.bot.logging.error(f"Failed to sync due to: {e}")
+            msg.edit(content="Unsuccesful sync")
+
+    @command(name="reload_cogs")
+    async def reload_cogs(self, ctx: discord.Interaction):
+        await ctx.response.defer()
+        msg = await ctx.followup.send("Reloading cogs...", wait=True, ephemeral=True)
+
+        self.bot.logging.info("updating cogs")
+
+        os.chdir("cogs/")
+        loaded = 0
+        for idx, cog in enumerate(glob.glob("*.py")):
+            if not cog.startswith("sync"):
+                try:
+                    self.bot.logging.info(f"Reloading {cog}")
+                    await self.bot.reload_extension(f"cogs.{cog[:-3]}")
+                    loaded += 1
+                except Exception as e:
+                    self.bot.logging.error(f"Unable to load {cog}, error of: {e}")
+
+        os.chdir("../")
+
+        await msg.edit(content="Sucessfully reloaded all cogs.")
+        self.bot.logging.info(f"Synced and reloaded {loaded}/{idx} cogs")
 
 
-async def setup(bot: commands.Bot):
-    await bot.add_cog(Sync(bot))
+async def setup(bot: MyDiscordBot):
+    await bot.add_cog(Refresh(bot))
